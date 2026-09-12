b1e5d64ed0ad53df22ea5bf518704d4a10edef0d
Harri Donaie <harridonaie@gmail.com>
2026-09-12 09:57:22 +0100

    2.3 - List Files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,9 @@
 import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
-
+    if directory == ".":
+        print("Result for current directory:")
+    else: print(f"Result for '{directory}' directory:")
     try:
 
         absolute_working_directory = os.path.abspath(working_directory)
@@ -10,12 +12,31 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         valid_target_dir = os.path.commonpath([absolute_working_directory, full_path]) == absolute_working_directory
 
         if not valid_target_dir:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return f'  Error: Cannot list "{directory}" as it is outside the permitted working directory'
         
-        if not os.path.isdir(directory):
-            return f'Error: "{directory}" is not a directory'
+        if not os.path.isdir(full_path):
+            return f'  Error: "{directory}" is not a directory'
         
-        return f'Success: "{directory}" is within the working directory'
+        directory_contents = os.scandir(full_path)
+        #print(str(directory_contents))
+        
+        files_info_list = []
+        
+        for file in directory_contents:
+            file_name = file.name
+            file_size = file.stat().st_size
+            file_isdir = file.is_dir()
+            """ print(file_name)
+            print(file_size)
+            print(file_isdir) """
+            
+            file_info = f"  - {file_name}: file_size={file_size} bytes, is_dir={file_isdir}"
+            files_info_list.append(file_info)
+        
+        return "\n".join(files_info_list)
+        """ return f'Success: "{directory}" is within the working directory' """
     
     except:
-        return "Error: Something went wrong outside of the planned parameters" 
+        return "  Error: Something went wrong outside of the planned parameters" 
+    
+    
