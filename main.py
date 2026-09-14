22ad6cff527598b39735034d528f0daa8363a4d5
Harri Donaie <harridonaie@gmail.com>
2026-09-14 08:46:25 +0100

    3.2 - Function Declaration

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import argparse
 from config import system_prompt
+from functions.call_function import available_functions
+import json
+
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
@@ -29,6 +32,7 @@ response = client.chat.completions.create(
     model = "openrouter/free",
     messages = messages,
     #temperature = 0,
+    tools = available_functions,
 )
 
 if response.usage is None:
@@ -38,4 +42,14 @@ if args.verbose == True:
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage.prompt_tokens}")
     print(f"Response tokens: {response.usage.completion_tokens}")
-print(response.choices[0].message.content)
+#print(response.choices[0].message.content)
+
+message = response.choices[0].message
+
+if message.tool_calls:
+    for tool_call in message.tool_calls:
+        function_args = json.loads(tool_call.function.arguments or "{}")
+        print(f"Calling function: {tool_call.function.name}({function_args})")
+    else:
+        print(message.content)
+        
